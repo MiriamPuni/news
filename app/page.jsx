f444dbc7129eae83a17af9bc71e7f81a93555db5
unknown <miriampuny16.com>
2024-05-22 13:54:03 +0300

    forms admin

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Mivzakim from '@/components/Mivzakim'
-export default function page() {
+import CardArticle from '@/components/CardArticle';
+import { getByOrdet, luli } from '@/server/BL/article.services';
+import connectToMongo from '@/server/DL/connectToMongo';
+import Image from 'next/image';
+// import React from 'react'
+import Footer from '@/components/Fotter'
+import Header from '@/components/Header'
+// import Mivzakim from '@/components/Mivzakim'
+
+export default async function page() {
+  await connectToMongo()
+  let data = await getByOrdet()
+  console.log('🥳',new Date(data[0].createDate).getHours());
   return (
-    <div>
-      <Mivzakim/>
+    <div className='container'>
+      <div style={{width:'100%', height:'300px', backgroundColor:'InactiveCaptionText'}}> </div>
+      <div style={{display:'flex'}}>
+<div style={{width:'30%', height:'500px', backgroundColor:'blue'}}><Mivzakim/></div>
+      <div className='containerCartArticle'>
+
+      {data.map((d, i)=> <CardArticle
+       slug={d.slug}
+       mainTitle={d.mainTitle} 
+       imgMain={d.imgMain} 
+       createDate={'kk'} />)}
+       </div>
+       </div>
     </div>
   )
 }
+
